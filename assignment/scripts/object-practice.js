743d87c615e33f422ea8d6fdf3236ca36f32dc43
Leo Johnson <leochinonejohnson@gmail.com>
2023-10-17 11:17:31 -0500

    Completed question 3.

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -33,8 +33,8 @@ console.log('My full name is', fullName);
   - Console.log your first favorite food
   - Console.log your last favorite food 
 */
-console.log('My first favorite food is', me.favThreeFoods[0]);
-console.log('My last favorite food is')
+console.log('My first favorite food is', me.favThreeFoods[0] + '.');
+console.log('My last favorite food is', me.favThreeFoods[2] + '.');
 
 
 /* 4. Change a property of an existing object.
